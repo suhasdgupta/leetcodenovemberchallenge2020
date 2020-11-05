0aed261dc71c9d29b5dc4c881aceff14bd2d4cde
Suhaas D <suhasdgupta55555@gmail.com>
2020-11-05 19:07:54 +0530

    Create MinimumCostToMoveChipsToTheSamePosition.java

---
+++ MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,9 @@
+class MinimumCostToMoveChipsToTheSamePosition {
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        for(int i:position)
+            if(i%2==0)
+                even++;
+        return Math.min(even,position.length-even);
+    }
+}
