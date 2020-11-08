75720092b7422cb94b8c194ca5bb4effae301e48
Suhaas D <suhasdgupta55555@gmail.com>
2020-11-08 14:39:15 +0530

    Create BinaryTreeTilt.java

---
+++ BinaryTreeTilt.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreeTilt {
+    int result = 0;
+    
+    public int findTilt(TreeNode root) {
+        postOrder(root);
+        return result;
+    }
+    
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+        
+        result += Math.abs(left - right);
+        
+        return left + right + root.val;
+    }
+}
